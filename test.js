c73eff5e37656c8947bb2e09f7e27cb56cc07045
Merlijn Vanherck <merlijn.vanherck@bignited.be>
2022-05-02 15:40:27 +0200

    Submit form

--- test.js
+++ test.js
@@ -1,5 +1,28 @@
-const webdriver = require('selenium-webdriver');
+const webdriver = require("selenium-webdriver");
+require("chromedriver");
+require("geckodriver");
 
-let driver = new webdriver.Builder()
-    .forBrowser(webdriver.Browser.CHROME)
+test();
+
+async function test() {
+  const url = "https://demoqa.com/automation-practice-form";
+  let driver = new webdriver.Builder()
+    .forBrowser(webdriver.Browser.FIREFOX)
     .build();
+
+  driver.get(url);
+
+  let form = { id: "userForm" };
+  let title = { css: "h5" };
+  let submit = { id: "submit" };
+
+  let firstNameInput = { id: "firstName" }
+  let lastNameInput = { id: "lastName" }
+
+  await driver.findElement(form).isDisplayed();
+  await driver.findElement(submit).click();
+
+  await driver.findElement(firstNameInput).sendKeys("Merlijn");
+  await driver.findElement(lastNameInput).sendKeys("Vanherck");
+  await driver.findElement(submit).click();
+};
